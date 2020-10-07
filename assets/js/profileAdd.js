9977610ab7e98e5346a23d7c88a6d9b0a7a96aee
Martin Hoffmann <71267302+martinh-dev@users.noreply.github.com>
2020-10-07 19:57:12 +0200

    added day counter

--- assets/js/profileAdd.js
+++ assets/js/profileAdd.js
@@ -40,10 +40,3 @@ $.getJSON("./data.json", (data) => {
     cardParent.append(card);
   }
 });
-
-$( document ).ready(function() {
-  var today = new Date();
-  var hacktoberfestends = new Date("2020-10-31");
-  var difference = hacktoberfestends.getTime() - today.getTime();
-  document.getElementById("numberofdaysleft").innerHTML = Math.floor(difference/(1000*3600*24));
-});
